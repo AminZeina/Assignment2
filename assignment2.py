290d2fdab7c9a7a258d283ae8f403bd1bc584f1c
Amin Zeina <amin.zeina@ocsbstudent.ca>
2018-03-01 13:56:28 -0500

    Initial commit

---
+++ assignment2.py
@@ -0,0 +1,17 @@
+radius = int(input("Please Enter the Radius: "))
+
+circumferenceOrArea = input('''
+Type "Area" to calculate area
+Type "Circumference" to calculate circumference
+''')
+
+if circumferenceOrArea == 'Area':
+	print( 3.14 * ( radius * radius ))
+
+elif circumferenceOrArea == 'Circumference' :
+    print( 2 * radius * 3.14 )
+
+else:
+	print('Invalid operation, please restart the program using "Area" or "Circumference"')
+
+input('End of Program')
